71b45daf1321375cd665ab2a9d2eeabb971758b7
photodiode <16804570+photodiode@users.noreply.github.com>
2021-04-04 20:06:04 +0900

    Update

--- js/code.js
+++ js/code.js
@@ -16,27 +16,113 @@ function newElement(name, attributes, children) {
 	return e;
 }
 
-function formatCode(codeNode) {
+function matchChar(src, matches) {
+	for (let i = 0; i < matches.length; i++) {
+		if (src[0] == matches[i]) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+function matchNumber(src) {
+	if (matchChar(src[0], ':!%()+,-*<=>[]{}|~^ \t\r\n') == 0) return 0;
+	let i = 1;
+	while (!isNaN(src.substr(1, i)) && i < src.length && matchChar(src[i], ' \t\r\n') == 0) {
+		i++;
+	}
+	return i-1;
+}
+
+function matchRegion(src, begin, end) {
+	if (src.substr(0, begin.length) == begin) {
+		for (let i = 1; i < src.length; i++) {
+			if (src.substr(i, end.length) == end || i == src.length-1) {
+				return i + end.length;
+			}
+		}
+	}
+	return 0;
+}
+
+function formatCode(preNode) {
+	
+	const codeNode = preNode.getElementsByTagName('code')[0];
 
 	const code = codeNode.textContent;
 
 	// line numbers
 	let lines = code.split('\n');
+	let lineNumbers = '';
 
 	if (lines[lines.length-1] == '') lines.pop();
-
-	let lineNumbers = '';
 
 	for(let i = 1; i <= lines.length; ++i){
 		lineNumbers += i+'\n';
 	}
 
-	//codeNode.innerHTML = '';
-	codeNode.prepend(newElement('div', {'content': lineNumbers, 'class': 'line_numbers'}));
-	//codeNode.appendChild(newElement('code', {'content': code}));
+	preNode.prepend(newElement('div', {'content': lineNumbers, 'class': 'line_numbers'}));
 	// ----
 
 	// high lighting
+	let output = '';
+	let i = 0;
 
+	while (i < code.length) {
+
+		let match, tmp, type;
+
+		tmp = matchChar(code.substr(i), ':!%&()+,-/.*<=>?[]{}|~^;');
+		if (tmp > 0) {
+			match = tmp;
+			type = 'symbol';
+		}
+
+		tmp = matchNumber(code.substr(i-1));
+		if (tmp > 0) {
+			match = tmp;
+			type = 'number';
+		}
+
+		// strings
+		tmp = matchRegion(code.substr(i), '"', '"');
+		if (tmp > 0) {
+			match = tmp;
+			type = 'string';
+		}
+
+		tmp = matchRegion(code.substr(i), '\'', '\'');
+		if (tmp > 0) {
+			match = tmp;
+			type = 'string';
+		}
+		
+		// comments
+		tmp = matchRegion(code.substr(i), '//', '\n');
+		if (tmp > 0) {
+			match = tmp;
+			type = 'comment';
+		}
+
+		tmp = matchRegion(code.substr(i), '/*', '*/');
+		if (tmp > 0) {
+			match = tmp;
+			type = 'comment';
+		}
+
+		if (match > 0) {
+			output += '<span class="' + type + '">' + code.substr(i, match) + '</span>';
+			i += match;
+		} else {
+			output += code[i];
+			i++;
+		}
+	}
+
+	codeNode.innerHTML = output;
 	// ----
+}
+
+function code() {
+	[...document.getElementsByTagName('pre')].forEach(formatCode);
 }
--- src/posts/this-blog.md
+++ src/posts/this-blog.md
@@ -1,2 +1,6 @@
 # This blog
 [2021.04.03]
+
+<video controls="true" allowfullscreen="true">
+	<source src="file:///home/arne/Videos/chip-8.mp4" type="video/mp4">
+</video>
