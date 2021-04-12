998d85f3bc1834560653478f6c3860e4cbb3ae85
photodiode <16804570+photodiode@users.noreply.github.com>
2021-04-12 16:23:37 +0900

    Update

--- publish.py
+++ publish.py
@@ -8,14 +8,48 @@ md = markdown.Markdown(output_format="html5", extensions=['extra']);
 website_name = 'photodiode'
 root = 'docs/'
 src_dir = 'src/articles/'
-files = os.listdir(src_dir)
 
+# template
 with open('src/template.html', 'r') as f:
 	template = f.read()
+
+def applyTemplate(title, content):
+	html = template.replace('<!-- TITLE -->', title , 1)
+	html = html.replace('<!-- CONTENT -->', content, 1)
+	return html
 # ----
 
-article_list = ''
+def lineNumbers(html):
 
+	output = ''
+	code = 0
+
+	for line in html.splitlines():
+
+		pre = ''
+
+		if not code:
+			match = re.search('<pre><code\s?([\S]+)?>', line)
+			if match:
+				pre  = line[0:match.span()[1]]
+				line = line[match.span()[1]:]
+				code = 1
+
+		else:
+			match = re.search('</code></pre>', line)
+			if match:
+				code = 0
+
+		if code:
+			output += pre + '<line>' + line + '</line>\n'
+		else:
+			output += line + '\n'
+
+	return output
+# ----
+
+files = os.listdir(src_dir)
+article_list = ''
 
 for filename in list(reversed(sorted(files))):
 
@@ -27,11 +61,14 @@ for filename in list(reversed(sorted(files))):
 
 	with open(src_dir+filename, 'r') as f:
 		text = f.read()
-		html = md.convert(text)
 	# ----
+	
+	
+	html = md.convert(text)
+	html = lineNumbers(html)
 
 	# title & date header
-	match = re.search("<h1>.*</h1>*", html)
+	match = re.search('<h1>.*</h1>*', html)
 	title = match.group()[4:-5]
 	date  = '<time datetime="' + date.replace('.', '-') + '">' + date + '</time>\n'
 	html  = html[0:match.span()[1]] + '\n' + date + html[match.span()[1]:]
@@ -39,8 +76,7 @@ for filename in list(reversed(sorted(files))):
 
 	# apply template
 	article = '<article>\n' + html + '\n</article>'
-	html = template.replace('<!-- TITLE -->', title + ' - ' + website_name, 1)
-	html = html.replace('<!-- CONTENT -->', article, 1)
+	html = applyTemplate(title + ' - ' + website_name, article)
 	# ----
 
 	newpath = 'article/' + name + '.html'
@@ -60,8 +96,7 @@ for filename in list(reversed(sorted(files))):
 print('making "index.html"...')
 
 article_list = '\t\t\t<h2>Articles</h2>\n\t\t\t<ol>\n' + article_list + '\t\t\t</ol>'
-html = template.replace('<!-- TITLE -->', website_name , 1)
-html = html.replace('<!-- CONTENT -->', article_list, 1)
+html = applyTemplate(website_name, article_list)
 
 with open(root + 'index.html', 'w') as f:
 	f.write(html)
